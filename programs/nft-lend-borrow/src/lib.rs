8a0354412335149c3633e6a3d0d5ab5736002f39
lilwuyn404 <161734123+lilwuyn404@users.noreply.github.com>
2024-02-29 22:47:28 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("2Bc29jJZAaT2eW78oj5s23YhUUhdNgFF7Y9kLoxD5uaf");
 
 #[program]
 pub mod nft_lend_borrow {
